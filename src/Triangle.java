c93d649eb476a8be050bfb94b316aff126d86fc0
jackS <jastajas@gamil.com>
2018-01-17 20:09:49 +0100

    Trójkąt - ver.1

---
+++ src/Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle {
+    double h;
+    double a;
+    double b;
+    double c;
+
+    public Triangle(double h, double a, double b, double c) {
+        this.h = h;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+}
