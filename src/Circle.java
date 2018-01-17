2b6a0f5d9f05340a4354d7426386011ccec487d6
jackS <jastajas@gamil.com>
2018-01-17 20:44:10 +0100

    Koło - ver.2 dodanie metod

--- src/Circle.java
+++ src/Circle.java
@@ -4,4 +4,11 @@ public class Circle {
     public Circle(double r) {
         this.r = r;
     }
+    double circleArea(){
+        return 3.14 * this.r * this.r;
+    }
+
+    double circlePerimeter(){
+        return 2 * 3.14 * this.r;
+    }
 }
